5da26b378663133b8c3a05e1f264fd389c7dd529
Kevin Palowa <kevinpalowa@gmail.com>
2023-02-25 19:09:48 +0700

    fix: callout responsive

--- components/Callout.tsx
+++ components/Callout.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import Button from "./Button";
+import useWindowSize from "@/hooks/useWindowSize";
 
 export const Callout = () => {
+  const { width } = useWindowSize();
   return (
-    <div className="flex border-y border-y-white/10 py-[47px]">
-      <h1 className="w-7/12 text-3xl font-bold leading-[48px]">
+    <div className="flex flex-col border-y border-y-white/10 py-[47px] text-center sm:flex-row sm:text-left">
+      <h1 className="text-3xl font-bold leading-[48px] sm:w-7/12">
         We&apos;ve prepared everything, it&apos;s time for you to tell the
         problem
       </h1>
-      <div className="flex w-5/12 items-center justify-end space-x-[10px]">
-        <Button color="primary">Send Quote</Button>
-        <Button color="white" style="light">
+      <div className="mt-12 flex items-center justify-center space-x-[10px] sm:mt-0 sm:w-5/12 sm:justify-end">
+        <Button color="primary" size={width > 671 ? "normal" : "sm"}>
+          Send Quote
+        </Button>
+        <Button
+          color="white"
+          style="light"
+          size={width > 671 ? "normal" : "sm"}
+        >
           Ask Us
         </Button>
       </div>
